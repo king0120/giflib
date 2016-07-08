dd0423e112b5257481c6cb1471a3cfc09e05cb04
jxk4066 <james_king@homedepot.com>
2016-07-08 16:16:02 -0400

    added page

--- src/main/java/com/kingjamie/giflib/controller/GifController.java
+++ src/main/java/com/kingjamie/giflib/controller/GifController.java
@@ -1,12 +1,23 @@
 package com.kingjamie.giflib.controller;
 
+import com.kingjamie.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
     @RequestMapping("/")
     public String listGifs(){
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Jamie King", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
